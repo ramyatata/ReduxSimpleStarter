3be153a4573ea2eadf1b79014d938b61946b9468
ramyatata <ramyatata007@gmail.com>
2018-01-10 15:50:36 -0800

    rendering hi

--- src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export default function selectBook(book) {
-  //selectBook is action creator and it returns action, which has type property and payload (more information about book)
-  return {
-    type: 'BOOK_SELECTED',
-    payload: book
-  };
-};
--- src/reducers/reducer-books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    {title: 'Good arts of Javascript!'},
-    {title: 'Eloquent JS'},
-    {title: 'Cracking coding Interview'},
-    {title: 'Harry potter'}
-  ];
-}
