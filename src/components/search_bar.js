643430f9147f29eb71c6be8cc22b278cddf583d0
ramyatata <ramyatata007@gmail.com>
2018-01-12 13:20:51 -0800

    search_bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {term: 'Enter search term... '}
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e){
+    this.setState({term: e.target.value});
+  }
+
   render() {
     return (
-      <input></input>
+      <input value={this.state.term} onChange = {this.handleInputChange}/>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTsearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar';
+import SearchBar from './components/search_bar.js';
 
 const API_KEY = 'AIzaSyD6ir1o1JP_lfHqcQwkMwW9yhJRNWsHiCE';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    this.state = {
+      videos: []
+    };
+
+    YTsearch({key: API_KEY, term: 'React'}, (data) => {
+      this.setState({videos: data});
+      console.log(data);
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
